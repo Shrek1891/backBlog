c512f9726b9011e17324ab39968db38c9753fe68
unknown <zhur_zhur@ukr.net>
2023-03-29 22:00:42 +0200

    chat

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.use(express.json())
 app.use("/", require("./router"))
 
 const server = require("http").createServer(app)
-const io = require("socket.io")(httpServer, {
+const io = require("socket.io")(server, {
+
   cors: {
     origin: "https://back-blog.vercel.app/",
     methods: ["GET", "POST"],
